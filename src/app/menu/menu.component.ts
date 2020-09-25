5b27c923ad5039637fb837963bd408023dfb3449
David Otero Diaz <daotdia@gmail.com>
2020-09-25 17:02:00 +0200

    Services básicos

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../share/dish';
-import { DISHES } from '../share/dishes'
+import {DishService} from '../services/dish.service'
 
 @Component({
   selector: 'app-menu',
@@ -9,17 +9,19 @@ import { DISHES } from '../share/dishes'
 })
 export class MenuComponent implements OnInit {
 
-  dishes = DISHES;
+  dishes: Dish[];
   
   selectedDish: Dish;
 
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   ngOnInit() { 
+    this.dishes = this.dishService.getDishes();
   }
 
   onSelect(dish: Dish) {
     this.selectedDish = dish;
   }
+
 
 }
---
+++ src/app/services/dish.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {Dish} from "../share/dish"
+import {DISHES} from "../share/dishes"
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DishService {
+
+  constructor() { }
+
+  getDishes(): Dish[]{
+    return DISHES;
+  }
+}
