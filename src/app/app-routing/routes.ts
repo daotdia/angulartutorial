a84f37bbf2e2888a1a1785398dc5b7e00cd61340
David Otero Diaz <daotdia@gmail.com>
2020-09-28 19:41:14 +0200

    Haciendo router Angular parametros

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -10,5 +10,6 @@ export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
   { path: 'contactus', component: ContactComponent},
+  { path: 'dishdetails/:id', component: DishdetailComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
